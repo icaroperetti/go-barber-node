c357dcb043e867d186727f4f891029eabcd4b680
Icaro Peretti <icaroperetti50@gmail.com>
2022-01-29 16:04:43 -0300

    Creating foreign key between appointment and User, creating user service and routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,1 +1,11 @@
-export {default as appointmentsRoutes} from './appointments.routes';
+import {Router} from 'express'
+
+import appointmentsRouter from './appointments.routes'
+import usersRouter from './users.routes'
+
+const routes = Router();
+
+routes.use('/appointments',appointmentsRouter)
+routes.use('/users',usersRouter)
+
+export default routes;
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,13 @@
 import 'reflect-metadata';
 import express from 'express';
-import { appointmentsRoutes } from './routes';
+import routes from './routes';
 import './database';
 
 
 const app = express();
 app.use(express.json());
 
-app.use("/appointments", appointmentsRoutes)
+app.use(routes)
 
 app.listen(3333, () => {
   console.log('Server started on port 3333');
