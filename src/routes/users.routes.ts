756c212c464b4fe25e9ca10862569174b7bda0b4
Icaro Peretti <icaroperetti50@gmail.com>
2022-01-29 18:27:09 -0300

    feat:Saving avatar filename to database

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,6 +1,7 @@
 import { response, Router } from 'express';
 import CreateUserService from '../services/CreateUserService';
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
 import multer from 'multer';
 import uploadConfig from '../config/upload';
 
@@ -29,8 +30,23 @@ usersRouter.post("/",async (request, response) => {
 
 
 usersRouter.patch("/avatar",ensureAuthenticated,upload.single('avatar'),async (request,response) => {
-  console.log(request.file);
-  return response.json({ok:true})
+ try {
+   const updateUserAvatar = new UpdateUserAvatarService();
+
+
+   const user = await updateUserAvatar.execute({
+      user_id: request.user.id,
+      // @ts-expect-error
+      avatarFilename: request.file.filename
+   })
+
+   // @ts-expect-error
+   delete user.password;
+   return response.json(user)
+
+ }catch(err:any){
+   return response.status(400).json({error:err.message});
+ }
 })
 
 export default usersRouter;
